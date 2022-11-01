c512095882db81dd278c87f1badca574a28a7572
test3teamapp <test3teamapp@gmail.com>
2022-11-01 13:53:03 +0000

    no socket.io with go coupling.  just simple golang upload  with http multipart from html form.

--- gouploadserver/uploadserver.go
+++ gouploadserver/uploadserver.go
@@ -5,12 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net/http"
-	"os"
-	"path/filepath"
-
-	socketio "github.com/googollee/go-socket.io"
+	"time"
 )
 
 const maxUploadSize = 2 * 1024 * 1024 // 2 MB
@@ -42,9 +38,14 @@ func uploadFileMultipart(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
+	if handler.Size > maxUploadSize {
+		fmt.Println("Error: File to big !")
+		return
+	}
+
 	// Create a temporary file within our temp-images directory that follows
 	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	tempFile, err := ioutil.TempFile(uploadPath, "upload-*.png")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -66,87 +67,33 @@ func uploadHtmlFrontend(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Html File Upload Frontend Hit")
 	http.ServeFile(w, r, "htmlupload.html")
 }
-func uploadFileSocketIO(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Html File Upload Frontend Hit")
-	http.ServeFile(w, r, "htmlupload.html")
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, time.Now().Format("02 Jan 2006 15:04:05 MST"))
+}
+
+func fileServerHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("fileServerHandlert : ", r.Host, r.URL.Path)
+
+	files, err := ioutil.ReadDir("./tmp")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	for _, file := range files {
+		fmt.Fprintf(w, "%s dir: %v  %d\n", file.Name(), file.IsDir(), file.Size())
+	}
 }
 
 func setupRoutes() {
 	http.HandleFunc("/uploadmultipart", uploadFileMultipart)
 	http.HandleFunc("/uploadfrontend", uploadHtmlFrontend)
-
-	server := socketio.NewServer(nil)
-
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
-
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-
-	server.OnEvent("/", "filesent", func(s socketio.Conn, file http.File) string {
-
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			return "INVALID_FILE"
-		}
-
-		detectedFileType := http.DetectContentType(fileBytes)
-		switch detectedFileType {
-		case "image/jpeg", "image/jpg":
-		case "image/gif", "image/png":
-		case "application/pdf":
-			break
-		default:
-			return "INVALID_FILE_TYPE"
-		}
-
-		fileName := randToken(12)
-		fileEndings, err := mime.ExtensionsByType(detectedFileType)
-		if err != nil {
-			return "CANT_READ_FILE_TYPE"
-		}
-		newPath := filepath.Join(uploadPath, fileName+fileEndings[0])
-		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
-
-		newFile, err := os.Create(newPath)
-		if err != nil {
-			return "CANT_CREATE_FILE"
-		}
-		defer newFile.Close()
-		if _, err := newFile.Write(fileBytes); err != nil {
-			return "CANT_WRITE_FILE"
-		}
-
-		fmt.Println("filesent:", "saved")
-		return "recv "
-	})
-
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
-	server.OnError("/", func(s socketio.Conn, e error) {
-		fmt.Println("meet error:", e)
-	})
-
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
-	})
-
-	go server.Serve()
-	defer server.Close()
-
-	http.Handle("/socket.io/", server)
-	http.HandleFunc("/", uploadHtmlFrontend) // http.FileServer(http.Dir("./asset")))
-	http.Handle("/nodejs", http.StripPrefix("/nodejs", http.FileServer(http.Dir("../node_modules/"))))
+	http.HandleFunc("/uploaddir", fileServerHandler) //http.FileServer(http.Dir("./tmp")))
+	http.HandleFunc("/servetime", timeHandler)
 	log.Println("Serving at localhost:8083...")
 	log.Fatal(http.ListenAndServe(":8083", nil))
 }
